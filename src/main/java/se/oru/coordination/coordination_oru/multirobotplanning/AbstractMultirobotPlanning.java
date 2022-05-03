9cad7df854406fbf457d5a1dfc49fbb917a2b1d2
Joao Salvado <joao.salvado@protonmail.com>
2022-05-03 15:39:22 +0200

    reading paths ongoing

--- src/main/java/se/oru/coordination/coordination_oru/multirobotplanning/AbstractMultirobotPlanning.java
+++ src/main/java/se/oru/coordination/coordination_oru/multirobotplanning/AbstractMultirobotPlanning.java
@@ -1,6 +1,8 @@
 package se.oru.coordination.coordination_oru.multirobotplanning;
 
 import com.google.common.collect.ObjectArrays;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.vividsolutions.jts.geom.Coordinate;
 import org.metacsp.multi.spatioTemporal.paths.Pose;
 import org.metacsp.multi.spatioTemporal.paths.PoseSteering;
@@ -11,12 +13,15 @@ import se.oru.coordination.coordination_oru.simulation2D.TrajectoryEnvelopeCoord
 import se.oru.coordination.coordination_oru.util.JTSDrawingPanelVisualization;
 import se.oru.coordination.coordination_oru.util.Missions;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashSet;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.*;
 import java.util.logging.Logger;
-import java.util.ArrayList;
+
 public  abstract class AbstractMultirobotPlanning {
+    protected Deque<PoseSteering[]>[] paths;
+    protected int[] mission_sequence_id;
     protected class MultirobotProblem{
         public MultirobotProblem(Pose[] start_, Pose[] goal_){
             this.start = start_; this.goal = goal_;
@@ -35,7 +40,14 @@ public  abstract class AbstractMultirobotPlanning {
             int R_, double max_vel_, double max_acc_, String map_file_){
         this.R = R_; this.max_vel = max_vel_; this.max_acc = max_acc_; this.map_file = map_file_;
         footprints = new ArrayList<>(R);
+        this.paths = (Deque<PoseSteering[]>[]) new Deque[R];
+        for(int r = 0; r < R; ++r){
+            this.paths[r] = new LinkedList<PoseSteering[]>();
+        }
+        mission_sequence_id = new int[R];
+        Arrays.fill(mission_sequence_id,0);
         tec = new TrajectoryEnvelopeCoordinatorSimulation(max_vel, max_acc);
+
     }
 
     public void addMultirobotProblem(Pose[] start_, Pose[] goal_){
@@ -68,6 +80,9 @@ public  abstract class AbstractMultirobotPlanning {
         Thread multirobotSolver = new Thread( ( ) -> {this.plan(this.problem);});
         multirobotSolver.start();
 
+        try{missionListener();}
+        catch(Exception e){}
+
 /*        for (int i = 1; i <= R; i++) {
             final int robotID = i;
             Thread t = new Thread() {
@@ -99,7 +114,50 @@ public  abstract class AbstractMultirobotPlanning {
         return true;
     }
 
-    boolean existsNewMission(int robotID){
+    class SimplePath{
+        boolean lastPath;
+        ArrayList<Double> x,y,z;
+        ArrayList<Double> roll, pitch, yaw;
+    }
+    private boolean missionListener() throws Exception {
+        Gson gson = new Gson();
+        ArrayList<Boolean> finished = new ArrayList<>(Arrays.asList(new Boolean[R]));
+        Collections.fill(finished, false);
+
+        while (true) {
+            // Listen for mission path files for each robot
+            for (int r = 0; r < R; ++r) {
+                if (finished.get(r)) continue; // Robot finished
+                String filename = "RH-DiMOpt/bin/t_path_" + r + "_" + mission_sequence_id[r]++;
+                File file = new File(filename);
+                // Check file
+                if (file.exists()) { // If file exists (path was computed)
+                    // Convert to simple_path
+                    SimplePath path = gson.fromJson(
+                            new FileReader(filename),
+                            SimplePath.class);
+                    // Convert to PoseSteering[]
+                    int N = path.x.size();
+                    PoseSteering[] ps = new PoseSteering[N];
+                    for (int k = 0; k < N; ++k) {
+                        ps[k] = new PoseSteering(
+                                path.x.get(k),
+                                path.y.get(k),
+                                path.yaw.get(k),
+                                0.0);
+                    }
+                    paths[r].addFirst(ps);
+                    finished.set(r, path.lastPath);
+                }
+            }
+            // Test if all robots finished
+            if (finished.stream().allMatch(elem -> elem)) break;
+
+        }
+        return true;
+    }
+
+/*    boolean existsNewMission(int robotID){
         // Check if file exists
 
         return false;
@@ -110,7 +168,7 @@ public  abstract class AbstractMultirobotPlanning {
 
 
         return  new Mission(robotID,path);
-    }
+    }*/
 
     /**
      * Get trajectory envelope coordinator and set it up
